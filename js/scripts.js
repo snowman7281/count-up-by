58e8e441ba36e1d274b4015479e1fd0920da8ac8
Kristen Kulha and snow vilay <snow@>
2017-08-15 09:14:40 -0700

    Add functionality to form and display results.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $("#counter").submit(function(event){
+
+    var countTo = parseInt($("#input1").val());
+    var countBy = parseInt($("#input2").val());
+    var result = [];
+    console.log(countTo);
+
+    if(isNaN(countTo) || isNaN(countBy)){
+       $("span").show();
+    }else{
+      for(var i = 0; i <= countTo; i += countBy){
+        result.push(i);
+      }
+    }
+
+    $("#output").text(result);
+    event.preventDefault();
+  });
+
+
+
+});
